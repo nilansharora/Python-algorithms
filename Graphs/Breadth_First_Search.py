ef38fab1cfa956e00118f44625ba305baa7e1066
Nilansh Arora <nilansh.arora@zomato.com>
2017-08-15 13:34:59 +0530

    python code

---
+++ Graphs/Breadth_First_Search.py
@@ -0,0 +1,35 @@
+class GRAPH:
+    """docstring for GRAPH"""
+    def __init__(self, nodes):
+        self.nodes=nodes
+        self.graph=[[0]*nodes for i in range (nodes)]
+        self.visited=[0]*nodes
+
+
+    def show(self):
+        print self.graph
+
+    def add_edge(self, i, j):
+        self.graph[i][j]=1
+        self.graph[j][i]=1
+
+    def bfs(self,s):
+        queue=[s]
+        self.visited[s]=1
+        while len(queue)!=0:
+            x=queue.pop(0)
+            print(x)
+            for i in range(0,self.nodes):
+                if self.graph[x][i]==1 and self.visited[i]==0:
+                    queue.append(i)     
+                    self.visited[i]=1
+
+n=int(input("Enter the number of Nodes : "))
+g=GRAPH(n)
+e=int(input("Enter the no of edges : "))
+print("Enter the edges (u v)")
+for i in range(0,e):
+    u,v=map(int, raw_input().split())
+    g.add_edge(u,v)
+s=int(input("Enter the source node :"))
+g.bfs(s)
